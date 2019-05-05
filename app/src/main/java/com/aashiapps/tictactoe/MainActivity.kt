29763d696a23fa4bfa9b112a164859aaa3a452b2
Aashish Gangwani <aashishgangwani@gmail.com>
2019-05-06 00:43:29 +0530

    Resolved some errors

--- app/src/main/java/com/aashiapps/tictactoe/MainActivity.kt
+++ app/src/main/java/com/aashiapps/tictactoe/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.aashiapps.tictactoe
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-    private val buttons = Array<Array<Button>>(3) { arrayOfNulls(3) }
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val player1Turn = true
+    private val buttons = Array<Array<Button?>>(3) { arrayOfNulls(3) }
+
+    private var player1Turn = true
+
     private var roundCount: Int = 0
 
-    private val player1Points: Int = 0
-    private val player2Points: Int = 0
+    private var player1Points: Int = 0
+    private var player2Points: Int = 0
 
     private var textViewPlayer1: TextView? = null
     private var textViewPlayer2: TextView? = null
@@ -28,21 +31,21 @@ class MainActivity : AppCompatActivity() {
         for (i in 0..2) {
             for (j in 0..2) {
                 val buttonID = "button_$i$j"
-                val resID = resources.getIdentifier(buttonID, "id",)
+                val resID = resources.getIdentifier(buttonID, "id", packageName)
                 buttons[i][j] = findViewById(resID)
-                buttons[i][j].setOnClickListener(this)
-
+                buttons[i][j]?.setOnClickListener(this)
             }
         }
 
         val buttonReset = findViewById<Button>(R.id.button_reset)
-        buttonReset.setOnClickListener { }
+        buttonReset.setOnClickListener { resetGame() }
     }
 
-    fun onClick(v: View) {
+    override fun onClick(v: View) {
         if ((v as Button).text.toString() != "") {
             return
         }
+
         if (player1Turn) {
             v.text = "X"
         } else {
@@ -50,14 +53,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         roundCount++
+
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins()
+            } else {
+                player2Wins()
+            }
+        } else if (roundCount == 9) {
+            draw()
+        } else {
+            player1Turn = !player1Turn
+        }
+
     }
 
     private fun checkForWin(): Boolean {
-        val field = Array<Array<String>>(3) { arrayOfNulls(3) }
+        val field = Array<Array<String?>>(3) { arrayOfNulls(3) }
+
         for (i in 0..2) {
             for (j in 0..2) {
-                field[i][j] = buttons[i][j].text.toString()
-
+                field[i][j] = buttons[i][j]?.text.toString()
             }
         }
 
@@ -69,15 +85,89 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         }
+
         for (i in 0..2) {
-            if (field[i][0] == field[i][1]
-                && field[i][0] == field[i][2]
-                && field[i][0] != ""
+            if (field[0][i] == field[1][i]
+                && field[0][i] == field[2][i]
+                && field[0][i] != ""
             ) {
                 return true
             }
         }
+
+        if (field[0][0] == field[1][1]
+            && field[0][0] == field[2][2]
+            && field[0][0] != ""
+        ) {
+            return true
+        }
+
+        return if (field[0][2] == field[1][1]
+            && field[0][2] == field[2][0]
+            && field[0][2] != ""
+        ) {
+            true
+        } else false
 
     }
 
+    private fun player1Wins() {
+        player1Points++
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show()
+        updatePointsText()
+        resetBoard()
+    }
+
+    private fun player2Wins() {
+        player2Points++
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show()
+        updatePointsText()
+        resetBoard()
+    }
+
+    private fun draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show()
+        resetBoard()
+    }
+
+    private fun updatePointsText() {
+        textViewPlayer1!!.text = "Player 1: $player1Points"
+        textViewPlayer2!!.text = "Player 2: $player2Points"
+    }
+
+    private fun resetBoard() {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                buttons[i][j]?.text = ""
+            }
+        }
+
+        roundCount = 0
+        player1Turn = true
+    }
+
+    private fun resetGame() {
+        player1Points = 0
+        player2Points = 0
+        updatePointsText()
+        resetBoard()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("roundCount", roundCount)
+        outState.putInt("player1Points", player1Points)
+        outState.putInt("player2Points", player2Points)
+        outState.putBoolean("player1Turn", player1Turn)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        roundCount = savedInstanceState.getInt("roundCount")
+        player1Points = savedInstanceState.getInt("player1Points")
+        player2Points = savedInstanceState.getInt("player2Points")
+        player1Turn = savedInstanceState.getBoolean("player1Turn")
+    }
 }
